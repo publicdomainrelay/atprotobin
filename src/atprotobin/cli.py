6768a4dd9f29d661acabc082e64b9d8e56ae9e5f
John Andersen <johnandersenpdx@gmail.com>
2025-03-20 16:38:41 -0700

    feat(cli): set bind host with env var
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/atprotobin/cli.py
+++ src/atprotobin/cli.py
@@ -6,6 +6,7 @@ import uvicorn
 async def async_main():
     config = uvicorn.Config(
         "atprotobin.server:app",
+        host=os.environ.get("HOST", "127.0.0.1"),
         port=int(os.environ.get("PORT", "8000")),
         log_level="debug",
     )
